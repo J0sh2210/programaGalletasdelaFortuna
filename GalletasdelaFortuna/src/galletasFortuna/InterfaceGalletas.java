b43740dfa7a507f5ebd08b11b03a6953eefa4220
J0sh2210 <jramireza31@miumg.edu.gt>
2025-02-07 22:14:57 -0600

    Creacion de interfaces

--- GalletasdelaFortuna/src/galletasFortuna/InterfaceGalletas.java
+++ GalletasdelaFortuna/src/galletasFortuna/InterfaceGalletas.java
@@ -1,2 +1,16 @@
-package galletasFortuna;public class InterfaceGalletas {
+package galletasFortuna;
+
+import javax.swing.*;
+
+public class InterfaceGalletas {
+    private JButton botton_cookie;
+    private JPanel Principanel;
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("InterfaceGalletas");
+        frame.setContentPane(new InterfaceGalletas().Principanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
 }
